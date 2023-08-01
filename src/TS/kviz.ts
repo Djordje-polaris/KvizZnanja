bbd573e8bcdc4eb055db3ab6d489d85732214f4d
Djordje-polaris <138766009+Djordje-polaris@users.noreply.github.com>
2023-08-01 16:16:33 +0200

    Zavrsena funkcionalnost aplikacije, ostalo picnuti

--- src/TS/kviz.ts
+++ src/TS/kviz.ts
@@ -30,29 +30,37 @@ function shuffleArray(array: number[]): number[] {
 }
 
 const category_name: HTMLHeadingElement = document.createElement("h2");
+category_name.setAttribute("id", "category_name");
 
 const question_container: HTMLDivElement = document.createElement("div");
+question_container.classList.add(
+  "question",
+  "border",
+  "background-blue-white",
+  "container1"
+);
 
 const question: HTMLHeadingElement = document.createElement("h2");
 question.setAttribute("id", "question");
 
 const answer_container: HTMLDivElement = document.createElement("div");
+answer_container.classList.add("container1","flex-answers");
+
+const answer_0: HTMLButtonElement = document.createElement("button");
+answer_0.classList.add("background-blue-white", "border");
+answer_0.setAttribute("id", "answer_0");
 
 const answer_1: HTMLButtonElement = document.createElement("button");
-answer_1.setAttribute("class", "button");
-answer_1.setAttribute("id", "answer_0");
+answer_1.classList.add("background-blue-white", "border");
+answer_1.setAttribute("id", "answer_1");
 
 const answer_2: HTMLButtonElement = document.createElement("button");
-answer_2.setAttribute("class", "button");
-answer_2.setAttribute("id", "answer_1");
+answer_2.classList.add("background-blue-white", "border");
+answer_2.setAttribute("id", "answer_2");
 
 const answer_3: HTMLButtonElement = document.createElement("button");
-answer_3.setAttribute("class", "button");
-answer_3.setAttribute("id", "answer_2");
-
-const answer_4: HTMLButtonElement = document.createElement("button");
-answer_4.setAttribute("class", "button");
-answer_4.setAttribute("id", "answer_3");
+answer_3.classList.add("background-blue-white", "border");
+answer_3.setAttribute("id", "answer_3");
 
 document.getElementById("main")?.appendChild(category_name);
 
@@ -60,10 +68,10 @@ document.getElementById("main")?.appendChild(question_container);
 question_container.appendChild(question);
 
 document.getElementById("main")?.appendChild(answer_container);
+answer_container.appendChild(answer_0);
 answer_container.appendChild(answer_1);
 answer_container.appendChild(answer_2);
 answer_container.appendChild(answer_3);
-answer_container.appendChild(answer_4);
 
 async function getData() {
   let difficultyJson = localStorage.getItem("difficulty");
@@ -71,11 +79,16 @@ async function getData() {
   let categoryJson = localStorage.getItem("categories");
   if (!categoryJson) return false;
 
+  let answeredJson = localStorage.getItem("answered");
+  if (!answeredJson) return false;
+
+  let answered: number = JSON.parse(answeredJson);
+  if (answered === 5) location.href = "./index.html";
+
   let categories_chosen: string[] = JSON.parse(categoryJson);
   if (!categories_chosen) return false;
 
   let randomNumber1 = Math.floor(Math.random() * categories_chosen.length);
-  console.log(randomNumber1);
 
   const category = categories_chosen[randomNumber1];
   console.log(category, difficultyJson);
@@ -105,41 +118,73 @@ async function getData() {
   let answers = incorrectAnswers;
   answers.push(correctAnswer);
 
-  let numberArray: number[] = shuffleArray([1, 2, 3, 4]);
+  let numberArray: number[] = shuffleArray([0, 1, 2, 3]);
+
+  let temp;
+
+  console.log(answers);
+
+  for (let i = 0; i < 4; i++) {
+    temp = answers[i];
+    answers[i] = answers[numberArray[i]];
+    answers[numberArray[i]] = temp;
+  }
 
   console.log(numberArray);
 
+  console.log(answers);
+
   for (let i = 0; i < 4; i++) {
-    let currentAnswer = document.getElementById("answer_" + i);
+    let currentAnswer = document.getElementById("answer_" + numberArray[i]);
     if (currentAnswer) currentAnswer.innerHTML = answers[i];
 
-    if(currentAnswer?.innerHTML === correctAnswer)
-    currentAnswer.setAttribute("class","correct")
-    else
-    currentAnswer?.setAttribute("class","incorrect")
-    console.log(currentAnswer?.innerHTML);
+    // if (currentAnswer?.innerHTML === correctAnswer)
+    //   currentAnswer.classList.add("correct");
+    // else currentAnswer?.classList.add("incorrect");
+    // console.log(currentAnswer?.innerHTML);
   }
+
+  const isCorrect = (answerDOM: string) => {
+    let answer = document.getElementById(answerDOM);
+    answered++;
+    localStorage.setItem("answered", JSON.stringify(answered));
+
+    if (answer?.innerHTML === correctAnswer) {
+      answer.classList.add("correct");
+      answer?.classList.remove("button");
+    } else {
+      answer?.classList.add("incorrect");
+      answer?.classList.remove("button");
+      for (let i = 0; i < 4; i++) {
+        let currentAnswer = document.getElementById("answer_" + i);
+        if (currentAnswer?.innerHTML === correctAnswer) {
+          currentAnswer.classList.add("correct");
+          currentAnswer.classList.remove("button");
+        }
+      }
+    }
+    setTimeout(() => {
+      location.href = "./kviz.html";
+    }, 2000);
+  };
 
   console.log("Correct answer is " + correctAnswer);
 
-  answer_1.addEventListener("click",()=>{
-    
-  })
+  answer_0.addEventListener("click", () => {
+    isCorrect("answer_0");
+  });
 
-  answer_2.addEventListener("click",()=>{
-    
-  })
+  answer_1.addEventListener("click", () => {
+    isCorrect("answer_1");
+  });
 
-  answer_3.addEventListener("click",()=>{
-    
-  })
+  answer_2.addEventListener("click", () => {
+    isCorrect("answer_2");
+  });
 
-  answer_4.addEventListener("click",()=>{
-    
-  })
-
+  answer_3.addEventListener("click", () => {
+    isCorrect("answer_3");
+  });
 }
-
-
 
 getData();
--- src/TS/menu.ts
+++ src/TS/menu.ts
@@ -1,36 +1,37 @@
 // empty then adding option based on button clicks
 const categories_chosen: string[] = [];
 let difficulty_chosen: string = "";
+localStorage.clear;
 
 const categories: HTMLDivElement = <HTMLDivElement>(
   document.createElement("div")
-  );
-  categories.setAttribute("class", "categories");
-  
-  const categories_h: HTMLHeadingElement = document.createElement("h2");
-  categories_h.textContent = "Categories";
-  
-  const music: HTMLButtonElement = document.createElement("button");
-  music.setAttribute("class", "button");
-  music.setAttribute("id", "music");
-  music.textContent = "Music";
-  
-  const hist: HTMLButtonElement = document.createElement("button");
-  hist.setAttribute("class", "button");
-  hist.setAttribute("id", "history");
-  hist.textContent = "History";
-  
-  const science: HTMLButtonElement = document.createElement("button");
-  science.setAttribute("class", "button");
-  science.setAttribute("id", "science");
-  science.textContent = "Science";
-  
-  const geography: HTMLButtonElement = document.createElement("button");
-  geography.setAttribute("class", "button");
-  geography.setAttribute("id", "geography");
-  geography.textContent = "Geography";
-  
-  document.querySelector("#main")?.appendChild(categories_h);
+);
+categories.classList.add("flex-space-evenly" , "container1");
+
+const categories_h: HTMLHeadingElement = document.createElement("h2");
+categories_h.textContent = "Categories";
+
+const music: HTMLButtonElement = document.createElement("button");
+music.classList.add("background-blue-white","border");
+music.setAttribute("id", "music");
+music.textContent = "Music";
+
+const hist: HTMLButtonElement = document.createElement("button");
+hist.classList.add("background-blue-white","border");
+hist.setAttribute("id", "history");
+hist.textContent = "History";
+
+const science: HTMLButtonElement = document.createElement("button");
+science.classList.add("background-blue-white","border");
+science.setAttribute("id", "science");
+science.textContent = "Science";
+
+const geography: HTMLButtonElement = document.createElement("button");
+geography.classList.add("background-blue-white","border");
+geography.setAttribute("id", "geography");
+geography.textContent = "Geography";
+
+document.querySelector("#main")?.appendChild(categories_h);
 document.querySelector("#main")?.appendChild(categories);
 categories.appendChild(music);
 categories.appendChild(hist);
@@ -38,23 +39,23 @@ categories.appendChild(science);
 categories.appendChild(geography);
 
 const difficulty: HTMLDivElement = document.createElement("div");
-difficulty.setAttribute("class", "difficulty");
+difficulty.classList.add("flex-space-evenly" , "container1");
 
 const difficulty_h: HTMLHeadingElement = document.createElement("h2");
 difficulty_h.textContent = "Difficulty";
 
 const easy: HTMLButtonElement = document.createElement("button");
-easy.setAttribute("class", "button");
+easy.classList.add("background-blue-white","border");
 easy.setAttribute("id", "easy");
 easy.textContent = "Easy";
 
 const medium: HTMLButtonElement = document.createElement("button");
-medium.setAttribute("class", "button");
+medium.classList.add("background-blue-white","border");
 medium.setAttribute("id", "medium");
 medium.textContent = "Medium";
 
 const hard: HTMLButtonElement = document.createElement("button");
-hard.setAttribute("class", "button");
+hard.classList.add("background-blue-white","border");
 hard.setAttribute("id", "hard");
 hard.textContent = "Hard";
 
@@ -73,33 +74,33 @@ let choose_category = (category: string) => {
   if (categories_chosen.length < 1) {
     categories_chosen.push(category);
     document
-    .getElementById(category)
-    ?.classList.replace("button", "tgl_button");
+      .getElementById(category)
+      ?.classList.add("tgl_button");
     console.log(categories_chosen);
     return;
   }
-  
+
   let flag = 0;
   for (let i = 0; i <= categories_chosen.length; i++) {
     if (category === categories_chosen[i]) flag++;
   }
-  
+
   if (flag)
-  for (let i = 0; i <= categories_chosen.length; i++) {
-    if (category === categories_chosen[i]) {
-      categories_chosen.splice(i, 1);
-      document
-      .getElementById(category)
-      ?.classList.replace("tgl_button", "button");
+    for (let i = 0; i <= categories_chosen.length; i++) {
+      if (category === categories_chosen[i]) {
+        categories_chosen.splice(i, 1);
+        document
+          .getElementById(category)
+          ?.classList.remove("tgl_button");
+      }
     }
-  }
   else {
     categories_chosen.push(category);
     document
-    .getElementById(category)
-    ?.classList.replace("button", "tgl_button");
+      .getElementById(category)
+      ?.classList.add("tgl_button");
   }
-  
+
   console.log(categories_chosen);
 };
 
@@ -107,20 +108,20 @@ let choose_difficulty = (difficulty: string) => {
   if (difficulty_chosen === "") {
     difficulty_chosen = difficulty;
     document
-    .getElementById(difficulty_chosen)
-    ?.classList.replace("button", "tgl_button");
+      .getElementById(difficulty_chosen)
+      ?.classList.add("tgl_button");
     console.log(difficulty_chosen);
     return;
   }
 
   if (difficulty_chosen !== difficulty) {
     document
-    .getElementById(difficulty_chosen)
-    ?.classList.replace("tgl_button", "button");
+      .getElementById(difficulty_chosen)
+      ?.classList.remove("tgl_button");
     difficulty_chosen = difficulty;
     document
-    .getElementById(difficulty_chosen)
-    ?.classList.replace("button", "tgl_button");
+      .getElementById(difficulty_chosen)
+      ?.classList.add("tgl_button");
 
     console.log(difficulty_chosen);
   }
@@ -152,7 +153,8 @@ hard.addEventListener("click", () => {
 });
 
 start.addEventListener("click", () => {
-  localStorage.setItem("categories",JSON.stringify(categories_chosen))
-  localStorage.setItem("difficulty",difficulty_chosen)
-  location.href="./kviz.html"
+  localStorage.setItem("categories", JSON.stringify(categories_chosen));
+  localStorage.setItem("difficulty", difficulty_chosen);
+  localStorage.setItem("answered", "0");
+  location.href = "./kviz.html";
 });
